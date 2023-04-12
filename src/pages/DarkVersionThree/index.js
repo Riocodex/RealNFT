930d5c9a8a6abc2a1253d817f08f9f04c69feafd
Rio <onwukachibike@gmail.com>
2023-04-12 14:18:33 +0100

    updated

--- src/pages/DarkVersionThree/index.js
+++ src/pages/DarkVersionThree/index.js
@@ -63,39 +63,56 @@ const DarkVersionThree = () => {
       console.log(error);
     }
   }
-  // const checkshii = async()=>{
-  //   const hello = await marketplace.getString()
-  //     console.log(hello)
-  // }
-  // const loadMarketplaceItems = async () => {
-  //   // Load all unsold items
-  //   console.log("this is marketplace",marketplace)
-  //   const itemCount = await marketplace.itemCount()
-  //   let items = []
-  //   for (let i = 1; i <= itemCount; i++) {
-  //     const item = await marketplace.items(i)
-  //     if (!item.sold) {
-  //       // get uri url from nft contract
-  //       const uri = await nft.tokenURI(item.tokenId)
-  //       // use uri to fetch the nft metadata stored on ipfs 
-  //       const response = await fetch(uri)
-  //       const metadata = await response.json()
-  //       // get total price of item (item price + fee)
-  //       const totalPrice = await marketplace.getTotalPrice(item.itemId)
-  //       // Add item to items array
-  //       items.push({
-  //         totalPrice,
-  //         itemId: item.itemId,
-  //         seller: item.seller,
-  //         name: metadata.name,
-  //         description: metadata.description,
-  //         image: metadata.image
-  //       })
-  //     }
-  //   }
+
+  const loadMarketplaceItems = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const signer = provider.getSigner();
+        const nft = new ethers.Contract(
+          nftAddress,
+          nftABI,
+          signer
+        );
+        const marketplace = new ethers.Contract(
+          marketplaceAddress,
+          marketplaceABI,
+          signer
+        );
+        const itemCount = await marketplace.itemCount()
+          let items = []
+          for (let i = 1; i <= itemCount; i++) {
+            const item = await marketplace.items(i)
+            if (!item.sold) {
+              // get uri url from nft contract
+              const uri = await nft.tokenURI(item.tokenId)
+              // use uri to fetch the nft metadata stored on ipfs 
+              const response = await fetch(uri)
+              const metadata = await response.json()
+              // get total price of item (item price + fee)
+              const totalPrice = await marketplace.getTotalPrice(item.itemId)
+              // Add item to items array
+              items.push({
+                totalPrice,
+                itemId: item.itemId,
+                seller: item.seller,
+                name: metadata.name,
+                description: metadata.description,
+                image: metadata.image
+              })
+            }
+          }
+          
+          setItems(items)
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
     
-  //   setItems(items)
-  // }
+  };
 
   // const buyMarketItem = async (item) => {
   //   await (await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })).wait()
