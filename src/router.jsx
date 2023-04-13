6a0a6f922bae42edc77026d08962660bf253f151
Rio <onwukachibike@gmail.com>
2023-04-13 14:50:52 +0100

    updated

--- src/router.jsx
+++ src/router.jsx
@@ -118,8 +118,8 @@ export default function Router() {
         <Route exact path="/creators" element={<Creator />} />
         <Route exact path="/wallet" element={<Wallet />} />
         <Route exact path="/activity" element={<Activity />} />
-        <Route exact path="/item-detail-one" element={<ItemDetailOne />} />
-        <Route exact path="/item-detail" element={<ItemDetailTwo/>} />
+        <Route exact path="/item-detail" element={<ItemDetailOne />} />
+        <Route exact path="/item-detail-two" element={<ItemDetailTwo/>} />
         <Route exact path="/auction" element={<Auction />} />
         <Route exact path="/explore-four" element={<ExploreFour />} />
         <Route exact path="/explore-three" element={<ExploreThree />} />
