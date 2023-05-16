fdb103454aaabd66630dec641b19aa4766cc6bf7
Rio <onwukachibike@gmail.com>
2023-05-16 22:04:35 +0100

    updated loadmarketplace items function

--- src/pages/CreatorProfile/index.js
+++ src/pages/CreatorProfile/index.js
@@ -240,6 +240,36 @@ const CreateProfile = () => {
           marketplaceABI,
           signer
         );
+        // Fetch purchased items from marketplace by quering Offered events with the buyer set as the user
+      const filter =  marketplace.filters.Bought(null,null,null,null,null,account)
+      const results = await marketplace.queryFilter(filter)
+      //Fetch metadata of each nft and add that to listedItem object.
+      const purchases = await Promise.all(results.map(async i => {
+        // fetch arguments from each result
+        i = i.args
+       
+        // get uri url from nft contract
+        const uri = await nft.tokenURI(i.tokenId)
+        // use uri to fetch the nft metadata stored on ipfs 
+        const response = await fetch(uri)
+        const metadata = await response.json()
+        // get total price of item (item price + fee)
+        const totalPrice = await marketplace.getTotalPrice(i.itemId)
+        // define listed item object
+        let purchasedItem = {
+          totalPrice,
+          price: i.price,
+          itemId: i.itemId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image
+        }
+        return purchasedItem
+      }))
+      setLoading(false)
+      setPurchases(purchases)
+    }
+
 
         
         
