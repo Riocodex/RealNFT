acced3277d8350f7a1daa79d52c2c0826918a9b8
Rio <onwukachibike@gmail.com>
2023-05-16 23:34:27 +0100

    created loadlisted items function

--- src/pages/CreatorProfile/index.js
+++ src/pages/CreatorProfile/index.js
@@ -279,6 +279,43 @@ const CreateProfile = ({currentAccount}) => {
     
   };
 
+  const loadListedItems = async () => {
+    // Load all sold items that the user listed
+    const itemCount = await marketplace2.itemCount()
+    let listedItems = []
+    let soldItems = []
+    for (let indx = 1; indx <= itemCount; indx++) {
+      const i = await marketplace2.items(indx)
+      if (i.seller.toLowerCase() === currentAccount) {
+        // get uri url from nft contract
+        const uri = await nft2.tokenURI(i.tokenId)
+        // use uri to fetch the nft metadata stored on ipfs 
+        const response = await fetch(uri)
+        const metadata = await response.json()
+        // get total price of item (item price + fee)
+        const totalPrice = await marketplace2.getTotalPrice(i.itemId)
+        // define listed item object
+        let item = {
+          totalPrice,
+          price: i.price,
+          itemId: i.itemId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image
+        }
+        listedItems.push(item)
+        // Add listed item to sold items array if sold
+        if (i.sold) soldItems.push(item)
+      }
+    }
+    
+    setListedItems(listedItems)
+    setSoldItems(soldItems)
+  }
+  useEffect(() => {
+    loadListedItems()
+  }, [])
+
   useEffect(() => {
     loadPurchasedItems()
   }, [])
